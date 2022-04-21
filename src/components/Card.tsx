236b3a6e0bf18da838532c633690683a73ede498
Alvarotloc <alvarotloc@gmail.com>
2022-04-21 20:25:26 +0200

    Apartado projects conseguido, perfectamente responsive y estilizado al máximo, próxima parada skills y we are done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,59 @@
-const Card = ():JSX.Element => {
-  return (
-    <div className="card">
-        
-    </div>
-  )
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+interface CardParams {
+  image: string;
+  name: string;
+  description: string;
+  usedSkills: string[];
+  repo: string;
+  url: string;
+  charging: boolean;
 }
 
-export default Card
+const Card = ({
+  image,
+  name,
+  description,
+  usedSkills,
+  repo,
+  url,
+  charging,
+}: CardParams): JSX.Element => {
+  return (
+    <div className="card">
+      {charging ? (
+        <Skeleton height={150} width={300} />
+      ) : (
+        <img
+          src={image}
+          alt="An image about the project"
+          className="img-top-card"
+        />
+      )}
+      <div className="card-content">
+        <h3>{charging ? <Skeleton /> : name}</h3>
+        <p>{charging ? <Skeleton /> : description}</p>
+        <div className="skills">
+          {usedSkills.map((usedSkill) => (
+            <div key={usedSkill} className="tech">
+              {charging ? <Skeleton /> : usedSkill}
+            </div>
+          ))}
+        </div>
+        {charging === false && (
+          <>
+            <a href={repo} target="_blank" className="btn-purple">
+              Repo
+            </a>
+            <a href={url} target="_blank" className="btn-blue">
+              Working
+            </a>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,25 +1,98 @@
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import { Project } from "../types/typeProject";
+
 const Projects = (): JSX.Element => {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [filtereds, setFiltereds] = useState<Project[]>([]);
+  const [filter, setFilter] = useState<string>("");
+  const [charging, setCharging] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const url = `http://localhost:4000/projects`;
+        const response = await fetch(url);
+        const result = await response.json();
+        setProjects(result);
+        // setTimeout(() => {
+        //   setCharging(false);
+        // }, 2000);
+        setCharging(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProjects();
+  }, []);
+
+  useEffect(() => {
+    if (filter.trim() === "") {
+      console.log("entro aqui");
+      return setFiltereds(projects);
+    }
+    setFiltereds(projects.filter((project) => filter === project.tech));
+  }, [filter]);
+
   return (
-      
-    <section id="top-section">
-      <h1 id="title">Projects</h1>
-      <p>
-        Most outstanding personal projects using web technologies, mostly
-        frameworks/libraries focused on javascript on their front-end side.
-      </p>
-      <div id="filter">
-        <label htmlFor="options" id="label-options">
-          Filter by category
-        </label>
-        <select name="options" id="options">
-          <option value="">-- All Projects --</option>
-          <option value="react">React</option>
-          <option value="tailwindcss">React + Tailwind</option>
-          <option value="basic">HTML and Css</option>
-          <option value="vue">Vue Js</option>
-        </select>
+    <>
+      <section id="top-section">
+        <h1 id="title">Projects</h1>
+        <p>
+          Most outstanding personal projects using web technologies, mostly
+          frameworks/libraries focused on javascript on their front-end side.
+        </p>
+        <div id="filter">
+          <label htmlFor="options" id="label-options">
+            Filter by category
+          </label>
+          <select
+            name="options"
+            id="options"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="">-- All Projects --</option>
+            <option value="react">React</option>
+            <option value="tailwindcss">React + Tailwind</option>
+            <option value="basic">HTML and Css</option>
+            <option value="vue">Vue Js</option>
+            <option value="vanilla">Vanilla JS</option>
+          </select>
+        </div>
+      </section>
+      <div id="cards">
+        {filtereds.length
+          ? filtereds.map(
+              ({ image, name, description, usedSkills, repo, url, id }) => (
+                <Card
+                  key={id}
+                  image={image}
+                  name={name}
+                  description={description}
+                  usedSkills={usedSkills}
+                  repo={repo}
+                  url={url}
+                  charging={charging}
+                />
+              )
+            )
+          : projects.map(
+              ({ image, name, description, usedSkills, repo, url, id }) => (
+                <Card
+                  key={id}
+                  image={image}
+                  name={name}
+                  description={description}
+                  usedSkills={usedSkills}
+                  repo={repo}
+                  url={url}
+                  charging={charging}
+                />
+              )
+            )}
       </div>
-    </section>
+    </>
   );
 };
 
