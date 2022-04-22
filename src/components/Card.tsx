246a2bdb14412ec363e8129f17eb0b9545c59821
Alvarotloc <alvarotloc@gmail.com>
2022-04-22 14:03:14 +0200

    test 2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -26,7 +26,7 @@ const Card = ({
         <Skeleton height={150} width={300} />
       ) : (
         <img
-          src={image}
+          src={`src/imgs/${image}.webp`}
           alt="An image about the project"
           className="img-top-card"
         />
